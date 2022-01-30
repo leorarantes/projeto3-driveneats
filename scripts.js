fe91557505f33512e97fdbbca880bc1570c3b0b9
Leonardo Arantes <leonardorarantes95@gmail.com>
2022-01-30 14:07:51 -0300

    Finish_Button_Version

--- scripts.js
+++ scripts.js
@@ -1,3 +1,16 @@
+function buttonReleased() {
+    const alreadySelectedDishe = document.querySelector(".dishes" + " " + ".selected");
+    const alreadySelectedDrink = document.querySelector(".drinks" + " " + ".selected");
+    const alreadySelectedDessert = document.querySelector(".desserts" + " " + ".selected");
+    const finishButton = document.querySelector(".finish_button");
+    const finishText = document.querySelector(".finish_button p:first-child");
+
+    if((alreadySelectedDishe !== null) && (alreadySelectedDrink !== null) && (alreadySelectedDessert !== null)) {
+        finishButton.classList.add("finish_button__green");
+        finishText.innerHTML = "Fechar pedido";
+    }
+}
+
 function select(selectedSection, selectedClass) {
     const alreadySelected = document.querySelector(selectedSection + " " + ".selected");
     const alreadyChecked = document.querySelector(selectedSection + " " + ".selected" + " " + ".check");
@@ -10,4 +23,6 @@ function select(selectedSection, selectedClass) {
     selectItem.classList.toggle("selected");
     const showCheck = document.querySelector(selectedSection + " " + selectedClass + " " + ".check");
     showCheck.classList.remove("hidden");
+
+    buttonReleased();
 }
